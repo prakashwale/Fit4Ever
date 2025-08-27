67f3a49f1954c3193cdb7a962d74e068a63cca61
prakashwale <kprajnly@gmail.com>
2025-08-27 19:28:23 -0400

    Day 1 commit

---
+++ src/main/java/com/example/fit4ever/Fit4EverApplication.java
@@ -0,0 +1,13 @@
+package com.example.fit4ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fit4EverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fit4EverApplication.class, args);
+	}
+
+}
